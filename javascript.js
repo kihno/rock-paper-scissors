dc90127acab68d858e3990881d3f306a3ca348a1
kihno <stistyle13@gmail.com>
2022-01-25 13:09:09 -0800

    completed javascript function for playRound

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 /* 
-Player inputs rock, paper, or scissors
 Computer randomly selects rock, paper, or scissors
+Player inputs rock, paper or scissors
 If player select equals computer selection, its a draw
 If player selects rock, computer wins with paper and loses with scissors
 If player selects paper, computer wins with scissors and loses with rock
 If player selects scissors, computer wins with rock and loses with paper
+
+Play 5 rounds
+If player wins >= 3, player wins
+If Player wins <= 2, player loses
 */
 
-let playerSelection = prompt("rock, paper, or scissors?", "");
+
 
 function computerPlay() {
     const selectionArray = ['rock', 'paper', 'scissors'];
@@ -15,14 +19,35 @@ function computerPlay() {
     return computerSelection;
 }
 
-let computerSelection = computerPlay();
+const computerSelection = computerPlay();
+const playerSelection = prompt("rock, paper, or scissors?", "");
 
-function playRound(computerSelection, playerSelection) {
-    if (computerSelection == playerSelection) {
-        alert("It's a tie!");
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        alert("You Lose!");
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        alert("You Win!");
+function playRound(playerSelection, computerSelection) {
+     if ((playerSelection.toLowerCase() === "rock") && (computerSelection == "paper")) {
+        return "You Lose! Paper beats rock.";
+    } else if ((playerSelection.toLowerCase() === "rock") && (computerSelection == "scissors")) {
+        return "You Win! Rock beats scissors.";
+    } else if ((playerSelection.toLowerCase() === "paper") && (computerSelection == "scissors")) {
+        return "You Lose! Scissors beat paper.";
+    } else if ((playerSelection.toLowerCase() === "paper") && (computerSelection == "rock")) {
+        return "You Win! Paper beats rock.";
+    } else if ((playerSelection.toLowerCase() === "scissors") && (computerSelection == "paper")) {
+        return "You Win! Scissors beat paper.";
+    } else if ((playerSelection.toLowerCase() === "scissors") && (computerSelection == "rock")) {
+        return "You Lose! Rock beats scissors.";
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+        return "It's a Tie!";
+    } else {
+        alert("Please enter rock, paper, or scissors");
     }
 }
+
+function game() {
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+}
+
+console.log(playRound(playerSelection, computerSelection));
