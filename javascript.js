46d40bea8d765c07a333c7ce46cfc6c0a5f58ae8
kihno <stistyle13@gmail.com>
2022-02-04 14:37:46 -0800

    added rps icon buttons and styling and updated robotisms

--- javascript.js
+++ javascript.js
@@ -1,22 +1,40 @@
-const results = document.querySelector('#results');
-const finalResult = document.querySelector('#finalResult');
-const playerScore = document.querySelector('#playerScore');
-const compScore = document.querySelector('#computerScore');
 const para = document.createElement('p');
+const results = document.querySelector('#results');
+const compScore = document.querySelector('#computerScore');
+const playerScore = document.querySelector('#playerScore');
+const finalResult = document.querySelector('#finalResult');
 
-const buttons = document.querySelectorAll('button');
+// Player input
+const buttons = document.querySelectorAll('input');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playRound(button.id, computerPlay());
     });
 });
 
+// Computer selection
 function computerPlay() {
     const selectionArray = ['rock', 'paper', 'scissors'];
     const selection = selectionArray[Math.floor(Math.random() * selectionArray.length)];
     return selection;
 }
 
+// Robotisms
+function robotWins() {
+    const winism = ['Pathetic. ', 'Silly Human. ', 'Inferior. ', 'Is that the best you got? ', 
+        'Your Choice was suboptimal. ', 'Haha loser. ', 'You lose, meat sack. '];
+    const robotSays = winism[Math.floor(Math.random() * winism.length)];
+    return robotSays;
+}
+
+function robotLose() {
+    const losism = ['How can this be? ', 'Are you cheating? ', `I'll get you next time. `,
+        'I let you win this round. ', 'Hmm it seems you won. ', 'Aaargh. ', 'This was only a temporary win. '];
+    const robotSays = losism[Math.floor(Math.random() * losism.length)];
+    return robotSays;
+}
+
+// Game logic
 let userScore = 0;
 let computerScore = 0;
 playerScore.textContent = userScore;
@@ -25,32 +43,32 @@ compScore.textContent = computerScore;
 function playRound(playerSelection, computerSelection) {
     
      if ((playerSelection == "rock") && (computerSelection == "paper")) {
-        para.textContent = "Silly human. Paper beats rock.";
+        para.textContent = robotWins() + "Paper beats rock.";
         para.setAttribute('id', 'roundResult');
         results.append(para);
         compScore.textContent = ++computerScore;
     } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
-        para.textContent = "You Win this round. Rock beats scissors.";
+        para.textContent = robotLose() + "Rock beats scissors.";
         para.setAttribute('id', 'roundResult');
         results.append(para);
         playerScore.textContent = ++userScore;
     } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
-        para.textContent = "Pathetic. Scissors beats paper.";
+        para.textContent = robotWins() + "Scissors beats paper.";
         para.setAttribute('id', 'roundResult');
         results.append(para);
         compScore.textContent = ++computerScore;
     } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
-        para.textContent = "You can have this one. Paper beats rock.";
+        para.textContent = robotLose() + "Paper beats rock.";
         para.setAttribute('id', 'roundResult');
         results.append(para);
         playerScore.textContent = ++userScore;
     } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
-        para.textContent = "I let you win this round. Scissors beats paper.";
+        para.textContent = robotLose() + "Scissors beats paper.";
         para.setAttribute('id', 'roundResult');
         results.append(para);
         playerScore.textContent = ++userScore;
     } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
-        para.textContent = "Inferior. Rock beats scissors.";
+        para.textContent = robotWins() + "Rock beats scissors.";
         para.setAttribute('id', 'roundResult');
         results.append(para);
         compScore.textContent = ++computerScore;
@@ -58,8 +76,6 @@ function playRound(playerSelection, computerSelection) {
         para.textContent = "It's a Tie!";
         para.setAttribute('id', 'roundResult');
         results.append(para);
-    } else {
-        return "Please enter rock, paper, or scissors";
     }
     
     if (userScore === 5) {
