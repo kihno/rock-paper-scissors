3fe7f553de39396ff7bd2602042d90d8588ab403
kihno <stistyle13@gmail.com>
2022-02-03 17:13:42 -0800

    game runs until a player reaches a score of 5

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
+const results = document.querySelector('#results');
+const finalResult = document.querySelector('#finalResult');
+const playerScore = document.querySelector('#playerScore');
+const compScore = document.querySelector('#computerScore');
+const para = document.createElement('p');
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playRound(button.id, computerPlay()));
+        playRound(button.id, computerPlay());
     });
 });
 
@@ -11,24 +17,76 @@ function computerPlay() {
     return selection;
 }
 
+let userScore = 0;
+let computerScore = 0;
+playerScore.textContent = "Player Score: " + userScore;
+compScore.textContent = "Computer Score: " + computerScore;
+
 function playRound(playerSelection, computerSelection) {
     
      if ((playerSelection == "rock") && (computerSelection == "paper")) {
-        return "You Lose. Paper beats rock.";
+        para.textContent = "You Lose. Paper beats rock.";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
+        compScore.textContent = "Computer Score: " + ++computerScore;
     } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
-        return "You Win. Rock beats scissors.";
+        para.textContent = "You Win. Rock beats scissors.";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
+        playerScore.textContent = "Player Score: " + ++userScore;
     } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
-        return "You Lose. Scissors beats paper.";
+        para.textContent = "You Lose. Scissors beats paper.";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
+        compScore.textContent = "Computer Score: " + ++computerScore;
     } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
-        return "You Win. Paper beats rock.";
+        para.textContent = "You Win. Paper beats rock.";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
+        playerScore.textContent = "Player Score: " + ++userScore;
     } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
-        return "You Win. Scissors beats paper.";
+        para.textContent = "You Win. Scissors beats paper.";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
+        playerScore.textContent = "Player Score: " + ++userScore;
     } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
-        return "You Lose. Rock beats scissors.";
+        para.textContent = "You Lose. Rock beats scissors.";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
+        compScore.textContent = "Computer Score: " + ++computerScore;
     } else if (playerSelection === computerSelection) {
-        return "It's a Tie!";
+        para.textContent = "It's a Tie!";
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
     } else {
         return "Please enter rock, paper, or scissors";
+    }
+    if (userScore === 5 || computerScore === 5) {
+        if (userScore === 5) {
+            para.textContent = `You Win the Game ${userScore} to ${computerScore}`;
+            finalResult.append(para);
+            buttons.forEach((button) => {
+                button.disabled = true;
+            });
+        } else {
+            para.textContent = `You Lose the Game ${userScore} to ${computerScore}`;
+            buttons.forEach((button) => {
+                button.disabled = true;
+            });
+            finalResult.append(para);
+        }
+    }
+}
+
+if (userScore === 5 || computerScore === 5) {
+    if (userScore === 5) {
+        para.textContent = "You Win the Game ", userScore, " to ", computerScore;
+        finalResult.append(para);
+        document.buttons.disabled = true;
+    } else {
+        para.textContent = "You Lose the Game ", userScore, " to ", computerScore;
+        document.buttons.disabled = true;
+        finalResult.append(para);
     }
 }
 
@@ -42,7 +100,9 @@ function game() {
        } else if (result.slice(0, 8) === "You Lose") {
            computerScore++;
        }
-       console.log(result);
+        para.textContent = result;
+        para.setAttribute('id', 'roundResult');
+        results.append(para);
    }
    if (userScore > computerScore) {
        console.log("You Win the Game ", userScore, " to ", computerScore);
